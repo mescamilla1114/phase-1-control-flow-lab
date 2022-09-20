133632b4ba57bf337cfc95f89a583c1f79c5c4e9
Michael <mescamilla1114@gmail.com>
2022-09-20 11:56:49 -0500

    commit tree 293d1b83b5ab34a27d29f62a05f812e6b5f12035

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-function scuberGreetingForFeet(){
+let someNumber;
+let response;
+let city;
+let rides;
+let tip;
+
+function scuberGreetingForFeet(rides, response){
   // Write your code here!
+  if (rides <= 200){
+    response = "This one is on me!";
+    return(response);
+
+  }
+
+  if (rides> 200 && rides <= 2000){
+    response = "That will be twenty bucks.";
+    return(response);
+  }
+
+  if (rides > 2000 && rides <=2500){
+    response = "I will gladly take your thirty bucks.";
+    return(response);
+
+  }
+
+  if (rides > 2500){
+    response = "No can do.";
+    return(response);
+
+  }
+
+ 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city, response){
   // Write your code here!
+  response = city == "NYC" ? "Ok, sounds good." : "No go.";
+  return(response);
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip, response){
   // Write your code here!
+  switch (tip) {
+    case 'generous': 
+      response = "Thank you so much.";
+      return(response);
+      break;
+
+    case 'not as generous':
+      response = "Thank you.";
+      return(response);
+      break;
+
+    default:
+      response = "Bye.";
+      return(response);
+      break;
+  }
 }
